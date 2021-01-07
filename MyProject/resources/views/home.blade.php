d2acff2862d8d4dd0cb001a4f9a92adee49cd0bd
Pietro <marrazzo.pietroalfonso@gmail.com>
2021-01-07 14:52:45 +0100

    banner complete

--- MyProject/resources/views/home.blade.php
+++ MyProject/resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 
 @section('content-main')
-    
+    <div class="bg-hero">
+        <div class="container hero p-relative">
+            <img class="p-absolute" src="{{ asset('images/cover-home.jpg') }}" alt="">
+        </div>
+    </div>
 @endsection
--- MyProject/resources/views/partials/header.blade.php
+++ MyProject/resources/views/partials/header.blade.php
@@ -61,10 +61,4 @@
                 </li>
             </ul>
         </div>
-        <div class="hero p-relative">
-            <div class="container">
-                <img class="p-absolute" src="{{ asset('images/cover-home.jpg') }}" alt="">
-            </div>
-
-        </div>
 </header>
